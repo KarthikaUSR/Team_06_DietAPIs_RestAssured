3489abbb492b446960edc7c481dece61afcb13d1
KarthikaUSR <Karthika.USR@gmail.com>
2024-08-16 12:14:10 -0700

    Common Method to fetch bearer token

---
+++ src/test/java/Utils/bearerTokenFetcher.java
@@ -0,0 +1,48 @@
+package Utils;
+import org.json.JSONObject;
+import org.junit.Assert;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import EndPoints.URL;
+import Payloads.userloginpojo;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class bearerTokenFetcher {
+	
+	 public static void fetchBearerToken(String authUrl,String username,String password) throws JsonProcessingException  {
+		 userloginpojo userobj = new userloginpojo();
+		 userobj.setUserLoginEmail(username);
+		 userobj.setPassword(password);
+		 ObjectMapper objectMapper = new ObjectMapper();
+		 String jsonbody = objectMapper.writeValueAsString(userobj);
+		//System.out.println(jsonbody);	
+	    Response response = RestAssured.given().contentType(ContentType.JSON)
+	        		.body(jsonbody)
+	                .when()
+	                .post(authUrl)
+	                .then().log().all()
+	        .extract().response();
+	    Assert.assertEquals(200, response.statusCode());
+	    String token=response.body().jsonPath().getString("token");
+	    System.out.println("BearerToken : "+token);
+	    userobj.setBearerToken(token);
+	    
+	        
+	        //return token;
+	    }
+
+	   /* public static void main(String[] args) throws JsonProcessingException  {
+	    
+	    	 String authUrl = URL.BaseURL+URL.LoginEndpoint;
+
+	        //String bearerToken = 
+	        fetchBearerToken(authUrl);
+	        //System.out.println("Bearer Token: " + bearerToken);
+	    }*/
+
+}
